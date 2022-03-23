3a90364f58627085d86cbc0f8c4ce8c7c1960d14
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2022-03-23 11:11:16 -0300

    Implementando primeira tela de estudo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,30 +15,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('BarbieTattoo'),
-          ),
-          body: Center(
-              child: Column(
-            children: <Widget>[
-              Text(
-                'Titulo',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 30,
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red,
+              title: Text(
+                'Targino Ink',
+                style: GoogleFonts.ramaraja(
+                  textStyle: TextStyle(color: Colors.white, fontSize: 30),
                 ),
               ),
-              Container(
-                child: Icon(
-                  Icons.star,
-                  size: 40,
-                  color: Colors.blue,
+            ),
+            body: (Column(
+              children: <Widget>[
+                Image(image: AssetImage('assets/Capturar.jpg')),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(children: [
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            'Cliente escolheu a palavra PRAGMÁTICO com a fonte gótica, duração de mais ou menos duas horas.',
+                            style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ))
+                      ],
+                    )),
+                  ]),
                 ),
-              ),
-              TextButton(onPressed: null, child: Text('Clique aqui'))
-            ],
-          ))),
-    );
+                Container(
+                  margin: EdgeInsets.only(bottom: 1),
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Text('Valor: 300',
+                          style: GoogleFonts.anton(
+                              textStyle:
+                                  TextStyle(color: Colors.black, fontSize: 16)))
+                    ],
+                  ),
+                )
+              ],
+            ))));
   }
 }
